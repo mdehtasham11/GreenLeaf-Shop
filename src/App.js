5661c462b373f3c1e6745398eef860bab0e6b6c9
mdehtasham11 <fostersam058@gmail.com>
2024-08-01 23:45:11 +0530

    Route Product

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React,{ useRef }  from 'react';
+import React, { useRef } from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 
-import Header from './components/Header';
-import Hero from './components/Hero';
-import FeaturedProducts from './components/FeaturedProducts';
-import Footer from './components/Footer';
-import './App.css';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import FeaturedProducts from "./components/FeaturedProducts";
+import Footer from "./components/Footer";
+import TShirts from "./pages/TShirts";
+import Toothbrush from "./pages/Toothbrush";
+import Shoppingbags from "./pages/ShoppingBags";
+
+import "./App.css";
 
 function App() {
   const featuredProductsRef = useRef(null);
-
   const scrollToFeaturedProducts = () => {
-    featuredProductsRef.current.scrollIntoView({ behavior: 'smooth' });
+    featuredProductsRef.current.scrollIntoView({ behavior: "smooth" });
   };
+
   return (
-    <div className="App">
-      <Header />
-      <Hero scrollToFeaturedProducts={scrollToFeaturedProducts} />
-      <FeaturedProducts ref={featuredProductsRef}/>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <MainContent scrollToFeaturedProducts={scrollToFeaturedProducts} featuredProductsRef={featuredProductsRef} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+function MainContent({ scrollToFeaturedProducts, featuredProductsRef }) {
+  const location = useLocation();
+
+  return (
+    <Routes>
+      <Route path="/" element={
+        <>
+          <Hero scrollToFeaturedProducts={scrollToFeaturedProducts} />
+          <FeaturedProducts ref={featuredProductsRef} />
+        </>
+      } />
+      <Route path="/product/3" element={<TShirts />} />
+      <Route path="/product/2" element={<Shoppingbags />} />
+      <Route path="/product/1" element={<Toothbrush />} />
+
+    </Routes>
   );
 }
 
--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,4 +1,5 @@
-import React , { forwardRef }from 'react';
+import React, { forwardRef } from 'react';
+import { Link } from 'react-router-dom';
 import './FeaturedProducts.css';
 
 const products = [
@@ -32,7 +33,9 @@ const FeaturedProducts = forwardRef((props, ref) => {
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p>{product.description}</p>
-            <button>View Product</button>
+            <Link to={`/product/${product.id}`}>
+              <button>View Product</button>
+            </Link>
           </div>
         ))}
       </div>
